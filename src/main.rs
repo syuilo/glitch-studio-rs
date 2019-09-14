29521f5204d441582317ceddaf02e640c57b8d2a
syuilo <Syuilotan@yahoo.co.jp>
2019-09-14 17:20:50 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,16 @@ const TEAR_MAX_TIMES: u32 = 32;
 const PIXEL_BLUR_AMOUNT: u32 = 256;
 const PIXEL_BLUR_FLUCTUATION: u32 = 32;
 
+macro_rules! cast_array {
+    ($array:expr, $castTo:ty, $size:expr) => {{
+        let mut arr: [$castTo; $size] = [0; $size];
+        for (to, from) in arr.iter_mut().zip($array.iter()) {
+           *to = *from as $castTo;
+        }
+        arr
+    }}
+}
+
 fn main() {
     let data = GSDataModel {
         fxs: vec!(FX {
@@ -44,7 +54,7 @@ fn main() {
 
     let path = path.trim();
 */
-    let path = "input.png";
+    let path = "input3.png";
 
     println!("Loading the image...");
 
@@ -71,6 +81,7 @@ fn main() {
     let img = pixel_blur(img, PIXEL_BLUR_AMOUNT, PIXEL_BLUR_FLUCTUATION);
     let img = granular(img);
     let img = tear(img, TEAR_MAX_TIMES, TEAR_MAX_HEIGHT, TEAR_AMOUNT);
+    let img = noise(img);
 
     img.save("output.png").unwrap();
 }
@@ -116,6 +127,26 @@ fn ghost(input: image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>, amount: 
 
     output
 }
+/*
+fn continurous_ghost(input: image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>) -> image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>> {
+    println!("ContinurousGhost FX");
+
+    let mut output = input.clone();
+    let (width, height) = input.dimensions();
+
+    for x in 0..width {
+        for y in 0..height {
+            let [_, src_g, _, _] = input.get_pixel(x - amount, y).0;
+            let [r, g, b, a] = input.get_pixel(x, y).0;
+
+            let dst_g = cmp::max(b, src_g);
+
+            output.put_pixel(x, y, image::Rgba([r, dst_g, b, a]));
+        }
+    }
+
+    output
+}*/
 
 fn tear(input: image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>, max_times: u32, max_height: u32, amount: u32) -> image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>> {
     println!("Tear FX");
@@ -148,6 +179,7 @@ fn pixel_blur(input: image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>, amo
     let mut output = input.clone();
     let (width, height) = input.dimensions();
     let mut rng = rand::thread_rng();
+    let fade = true;
 
     for x in 0..width {
         for y in 0..height {
@@ -156,20 +188,20 @@ fn pixel_blur(input: image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>, amo
             let intensity = ((r as f32) + (g as f32) + (b as f32)) / (255 * 3) as f32;
             let velocity = (intensity * amount as f32).floor() as u32;
             let velocity = velocity + rng.gen_range(0, fluctuation);
-            for i in 0..velocity {
-                if _y + i < height {
-                    output.put_pixel(x, _y + i, image::Rgba([r, g, b, a]));
-
-                    /*output.put_pixel(x, _y + i, blend(
-                        *input.get_pixel(x, _y + i),
-                        image::Rgba([r, g, b, a]),
-                        (i as f32) / (velocity as f32)));*/
-
-                    /*let [r2, g2, b2, a2] = img.get_pixel(x, _y + i).0;
-                    let opacity = (((i as f32) / (velocity as f32)) * 255 as f32) as u8 / 2;
-                    let new_p = image::Rgba([
-                        cmp::max(r, r2), cmp::max(g, g2), cmp::max(b, b2), 255]);
-                    out.put_pixel(x, _y + i, new_p);*/
+            if fade {
+                for i in 0..velocity {
+                    if _y + i < height {
+                        output.put_pixel(x, _y + i, blend(
+                            *input.get_pixel(x, _y + i),
+                            image::Rgba([r, g, b, a]),
+                            (i as f32) / (velocity as f32)));
+                    }
+                }
+            } else {
+                for i in 0..velocity {
+                    if _y + i < height {
+                        output.put_pixel(x, _y + i, image::Rgba([r, g, b, a]));
+                    }
                 }
             }
         }
@@ -217,14 +249,44 @@ fn granular(input: image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>) -> im
     output
 }
 
+fn noise(input: image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>) -> image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>> {
+    println!("Noise FX");
+
+    let mut output = input.clone();
+    let (width, height) = input.dimensions();
+    let times = 2048;
+    let velocity = 128;
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..times {
+        let noise_x = rng.gen_range(0, width);
+        let noise_y = rng.gen_range(0, height);
+        let px = input.get_pixel(noise_x, noise_y);
+
+        for i in 0..velocity {
+            let dst_x = noise_x + i;
+            let dst_y = noise_y;
+
+            if dst_x >= width { continue; }
+
+            output.put_pixel(dst_x, dst_y, blend(
+                image::Rgba(input.get_pixel(dst_x, dst_y).0),
+                image::Rgba(px.0),
+                1 as f32 - (i as f32 / velocity as f32) as f32
+            ));
+        }
+    }
+
+    output
+}
+
 fn blend(dst: Rgba<u8>, src: Rgba<u8>, mix: f32) -> image::Rgba<u8> {
-    let mix: u8 = (mix * (255 as f32)).floor() as u8;
-    let [r1, g1, b1, a1] = dst.0;
-    let [r2, g2, b2, a2] = src.0;
+    let mix: u16 = (mix * (255 as f32)).floor() as u16;
+    let [r1, g1, b1, a1] = cast_array!(src.0, u16, 4);
+    let [r2, g2, b2, a2] = cast_array!(dst.0, u16, 4);
     let r = (r1 * mix + r2 * (255 - mix)) / 255;
     let g = (g1 * mix + g2 * (255 - mix)) / 255;
     let b = (b1 * mix + b2 * (255 - mix)) / 255;
     let a = (a1 * mix + a2 * (255 - mix)) / 255;
-    println!("{} {} {} {}", r, g, b, a);
-    image::Rgba([r, g, b, a])
+    image::Rgba([r as u8, g as u8, b as u8, a as u8])
 }
